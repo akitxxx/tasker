62e43417ccba6d68af53eac389d6cab23b51896a
aki <>
2020-04-01 01:17:45 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/lelouch99v/booker
+
+go 1.13
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/labstack/echo/v4 v4.1.15
+	golang.org/x/crypto v0.0.0-20200317142112-1b76d66859c6 // indirect
+	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+	golang.org/x/sys v0.0.0-20200317113312-5766fd39f98d // indirect
+	google.golang.org/api v0.20.0
+)
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "net/http"
+	"github.com/labstack/echo/v4"
+)
+
+const port = "5001"
+
+type AuthInfo struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func main() {
+	e := echo.New()
+
+	initRouting(e)
+
+	e.Logger.Fatal(e.Start(":" + port))
+}
+
+func initRouting(e *echo.Echo) {
+	e.GET("/", index)
+	e.POST("/auth", auth)
+}
+
+func index(c echo.Context) error {
+	return c.String(http.StatusOK, "hello, tasker!")
+}
+
+func auth(c echo.Context) error {
+	authInfo := new(AuthInfo)
+	if err := c.Bind(authInfo); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, authInfo)
+}
