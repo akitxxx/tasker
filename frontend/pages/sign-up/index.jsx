f285b37d8fe0853a33fce5917f0dab0da80e679d
aki <>
2020-11-15 19:09:50 +0900

    Update

--- frontend/pages/sign-up/index.jsx
+++ frontend/pages/sign-up/index.jsx
@@ -1,19 +1,12 @@
 import React, { useState } from 'react'
-import '../styles/login.scss'
 import {Container, Row, Col, Form, Button} from 'react-bootstrap'
 import axios from 'axios'
 
-const SignUp = () => {
+const SignUpPage = (props) => {
+  const [params, setParams] = useState({})
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
-  const handleEmailChange = e => {
-    setEmail(e.target.value)
-  }
-
-  const handlePasswordChange = e => {
-    setPassword(e.target.value)
+  const handleChange = e => {
+    setParams({ ...params, [e.target.name]: e.target.value })
   }
 
   const handleKeyPress = e => {
@@ -21,40 +14,39 @@ const SignUp = () => {
     if (e.keyCode === ENTER) {
       submit()
     }
-  }
+  };
 
   const submit = async () => {
-    const uri = '/api/auth'
+    const uri = '/api/sign-up'
     try {
-      const res = await axios.post(uri, {
-        email: email,
-        password: password
-      })
-      alert(`ログイン成功\nemail: ${res.data}\n`)
-      window.location.href = '/task-board'
-    } catch (err) {
-      alert('ログイン失敗\n' + err)
+      const res = await axios.post(uri, params)
+      // alert(res)
+    } catch(e) {
+      // alert('error')
     }
   }
 
   return (
-    <Container className='login'>
+    <Container className='sign-up'>
       <Row>
         <Col className='text-center my-3'>
-          <h2>ログイン</h2>
+          <h2>Sign up</h2>
         </Col>
       </Row>
       <Row>
         <Col>
           <Form className='col-md-6 offset-md-3 col-sm-8 offset-sm-2 col-xs-8 offset-xs-2'>
             <Form.Group>
-              <Form.Control onChange={handleEmailChange} onKeyDown={handleKeyPress} type='text' name='id' placeholder='e-mail' />
+              <Form.Control onChange={handleChange} onKeyDown={handleKeyPress} type='text' name='id' placeholder='ID' />
             </Form.Group>
             <Form.Group>
-              <Form.Control onChange={handlePasswordChange} onKeyDown={handleKeyPress} type='password' name='password' placeholder='password' />
+              <Form.Control onChange={handleChange} onKeyDown={handleKeyPress} type='password' name='password' placeholder='Password' />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control onChange={handleChange} onKeyDown={handleKeyPress} type='password' name='confirmPassword' placeholder='Confirm password' />
             </Form.Group>
             <div className='text-center'>
-              <Button onClick={submit} className='w-100'>ログイン</Button>
+              <Button onClick={submit} className='w-100'>Sign up</Button>
             </div>
           </Form>
         </Col>
@@ -63,4 +55,4 @@ const SignUp = () => {
   )
 }
 
-export default SignUp
+export default SignUpPage
