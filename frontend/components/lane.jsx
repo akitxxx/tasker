2c4f8ea4e34987d6d9208506ff841d7f995ff54a
aki <>
2021-01-14 00:48:19 +0900

    Add task input form and add/cancel button

--- frontend/components/lane.jsx
+++ frontend/components/lane.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Card from './card';
 import './lane.module.scss';
+import { Form, Button } from 'react-bootstrap';
 
 const Lane = (props) => {
+
+  const [showInput, setShowInput] = useState(false);
+  const taskInput = useRef(null);
+
+  useEffect(() => {
+    showInput && taskInput.current.focus();
+  }, [showInput]);
+
+  const handleClickAddCard = (e) => {
+    e.target.blur();
+    setShowInput(true);
+  };
+
+  const handleClickCancel = () => {
+    setShowInput(false);
+  };
+
+  const handleClickAdd = () => {
+    setShowInput(false);
+  };
 
   return (
     <div className="lane col-md-4">
@@ -13,6 +34,16 @@ const Lane = (props) => {
         return <Card key={task.id} title={task.title} />;
       })}
       <div className="laneFooter">
+        {showInput &&
+        <Form className="taskInputForm">
+          <Form.Control type="text" ref={taskInput} />
+            <Button className="mr-2" onClick={handleClickAdd}>Add</Button>
+            <Button variant="" onClick={handleClickCancel}>Cancel</Button>
+        </Form>
+        }
+
+        {!showInput &&
+        <button type="button" className="btn btnAddCard" onClick={handleClickAddCard}>+ Add card</button>}
       </div>
     </div>
   );
