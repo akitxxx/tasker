dfab3b0cfc79b5cd20a8b15e19694ed7edc1fcf3
aki <>
2021-01-26 19:52:31 +0900

    Add lane table

--- db/setup.sql
+++ db/setup.sql
@@ -18,16 +18,31 @@ values (
   "password"
 );
 
+create table lanes (
+  id integer primary key auto_increment,
+  name varchar(50) not null,
+  created_at datetime default current_timestamp,
+  updated_at  timestamp default current_timestamp on update current_timestamp
+);
+
+insert into lanes (id, name)
+values (
+  1,
+  "tasks1"
+);
+
 create table tasks (
   id integer primary key auto_increment,
+  lane_id integer not null,
   title varchar(255) not null,
   content varchar(100),
   created_at datetime default current_timestamp,
   updated_at timestamp default current_timestamp on update current_timestamp
 );
 
-insert into tasks (title, content)
+insert into tasks (lane_id, title, content)
 values (
+  1,
   "task1",
   "task1 content"
 );
