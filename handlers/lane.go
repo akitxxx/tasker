dc319eb0d034768653f8186f3f0038f0c04aa9ee
aki <>
2021-02-04 21:19:15 +0900

    Add create lane api

--- handlers/lane.go
+++ handlers/lane.go
@@ -1,1 +1,35 @@
 package handlers
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/lelouch99v/tasker/models"
+)
+
+func CreateLane(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.NotFound(w, r)
+		return
+	}
+
+	// get request body
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		renderError(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	// json parse
+	newLane := models.Lane{}
+	err = json.Unmarshal(body, &newLane)
+	if err != nil {
+		log.Println(err)
+		renderError(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	lane, err := models.CreateLane(&newLane)
+}
--- models/lane.go
+++ models/lane.go
@@ -11,6 +11,24 @@ type Lane struct {
 	TaskList  []Task    `json:"task_list"`
 }
 
+func FindLaneById(id uint64) (*Lane, error) {
+	var db, _ = DbConn()
+
+	sql := "select * from lanes where id = ?;"
+	stmt, err := db.Prepare(sql)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	lane := Lane{}
+	if err := stmt.QueryRow(id).Scan(&lane.UserId, &lane.Name, &lane.CreatedAt, &lane.UpdatedAt); err != nil {
+		return nil, err
+	}
+
+	return &lane, nil
+}
+
 func SelectLaneList() ([]Lane, error) {
 	var db, _ = DbConn()
 
@@ -36,4 +54,27 @@ func SelectLaneList() ([]Lane, error) {
 	}
 
 	return lanes, nil
+}
+
+func CreateLane(lane *Lane) (*Lane, error) {
+	var db, _ = DbConn()
+
+	sql := "insert into lanes(user_id, name, created_at, updated_at) values(?, ?, ?, ?)"
+	stmt, err := db.Prepare(sql)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	// insert
+	res, err := stmt.Exec(lane.UserId, lane.Name, lane.CreatedAt, lane.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+
+	// get inserted task
+	id, _ := res.LastInsertId()
+	newLane, err := FindLaneById(uint64(id))
+
+	return newLane, nil
 }
