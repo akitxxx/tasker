05d1facb946f9edbd85f04a3ae1b5b36e39c0b91
aki <>
2021-02-11 00:52:23 +0900

    Use modal when user add lane

---
+++ frontend/components/modal.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+const ModalDialog = (props) => {
+
+  return (
+    <Modal show={props.show} onHide={props.onHide}>
+      <Modal.Header closeButton>
+        <Modal.Title>{props.title}</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        {props.children}
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="secondary" onClick={props.onCancel}>Cancel</Button>
+        <Button variant="primary" onClick={props.onExec}>{props.btnExecName}</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ModalDialog;
--- frontend/pages/task-board/index.jsx
+++ frontend/pages/task-board/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Router from 'next/router';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button, Form } from 'react-bootstrap';
+import ModalDialog from '../../components/modal';
 import Lane from '../../components/lane';
 import axios from 'axios';
 import { useEffect } from 'react';
@@ -10,6 +11,8 @@ import '../../styles/task-board.scss';
 const TaskBoard = () => {
 
   const [laneList, setLaneList] = useState([]);
+  const [showAddLaneModal, setShowAddLaneModal] = useState(false);
+  const [laneName, setLaneName] = useState(null);
 
   useEffect(() => {
     // componentDidMount
@@ -34,15 +37,18 @@ const TaskBoard = () => {
       }
   };
 
-  const handleClickBtnAddLane = async (e) => {
+  const handleClickBtnAddLane = (e) => {
     e.preventDefault();
+    setShowAddLaneModal(true);
+  };
 
+  const createLane = async (name) => {
     const uri = '/api/create-lane';
     // get token from local storage
     const token = localStorage.getItem('tasker_token');
     try {
       const res = await axios.post(uri, {
-
+        name: name,
       }, {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -52,7 +58,10 @@ const TaskBoard = () => {
       fetchTaskList();
     } catch(e) {
       alert(e);
+      return;
     }
+
+    setShowAddLaneModal(false);
   };
 
   return (
@@ -66,6 +75,17 @@ const TaskBoard = () => {
           <Col md={2}><Button variant="default" className="btnAddLane" onClick={handleClickBtnAddLane}>+ Add lane</Button></Col>
         </Row>
       </Container>
+      <ModalDialog
+        title="Add lane"
+        btnExecName="Add lane"
+        show={showAddLaneModal}
+        onHide={() => setShowAddLaneModal(false)}
+        onCancel={() => setShowAddLaneModal(false)}
+        onExec={() => createLane(laneName)}
+      >
+        <Form.Label>Lane name</Form.Label>
+        <Form.Control type="text" onChange={(e) => {setLaneName(e.target.value)}} />
+      </ModalDialog>
     </Layout>
   );
 };
